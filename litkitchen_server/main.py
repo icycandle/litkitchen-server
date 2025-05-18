9ab63f29094b4bd892b32acc43349d8bbf77ddac
icycandle <icycandle@gmail.com>
2025-05-19 02:59:32 +0800

    fix(api): CORS dev solution

--- litkitchen_server/main.py
+++ litkitchen_server/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from litkitchen_server.api.routers import router as api_router
 from litkitchen_server import settings
 import sentry_sdk
+from fastapi.middleware.cors import CORSMiddleware
 
 sentry_sdk.init(
     dsn=settings.SENTRY_DSN,
@@ -9,6 +10,16 @@ sentry_sdk.init(
 )
 
 app = FastAPI(title="litkitchen-server")
+
+# 加入 CORS middleware 允許前端 dev server 跨域
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # 啟動時插入 options fixture
 try:
